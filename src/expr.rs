14d2411e258a65e7ae5ba8c8bae9ace43cab3352
Emirhan TALA <tala.emirhan@gmail.com>
2024-06-24 03:22:22 +0300

    i forgor commit

--- src/expr.rs
+++ src/expr.rs
@@ -24,28 +24,3 @@ pub enum Expr {
         value: Box<Expr>,
     },
 }
-
-pub enum Stmt {
-    Expression {
-        expression: Expr,
-    },
-    Print {
-        expression: Expr,
-    },
-    Var {
-        name: Token,
-        initializer: Option<Expr>,
-    },
-}
-
-impl Into<Option<Expr>> for Stmt {
-    fn into(self) -> Option<Expr> {
-        match self {
-            Stmt::Expression { expression } | Stmt::Print { expression } => Some(expression),
-            Stmt::Var {
-                name: _,
-                initializer,
-            } => initializer,
-        }
-    }
-}
